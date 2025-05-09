69b970ae7def5b79dfe78442023d09f95d0c0baa
levnikonenko <levnikonenko@yandex.ru>
2025-05-09 16:19:02 +0300

    fix Подправил код, удалил лишнее

--- src/main/java/fun/justdevelops/otpservice/rest/AdminController.java
+++ src/main/java/fun/justdevelops/otpservice/rest/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/login")
 public class AdminController {
 
     private final OtpConfigService otpConfigService;
